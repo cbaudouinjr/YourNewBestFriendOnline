82191aabcbde7bd723f9561b2df67bf39aaf3a81
Elliot Miller <elliot@bitoffdev.com>
2018-02-17 20:42:53 -0500

    Update route to webapp

--- YourNewBestFriendOnline/urls.py
+++ YourNewBestFriendOnline/urls.py
@@ -17,6 +17,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('webapp/', include('webapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('webapp.urls')),
 ]
